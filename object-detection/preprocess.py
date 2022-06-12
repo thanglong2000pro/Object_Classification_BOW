0b1dabf256418bb9f947e1c6d1f32e37f5d07492
thanglong2000pro <laphungtien2000@gmail.com>
2022-06-12 11:55:16 +0700

    add command line gen folder

--- object-detection/preprocess.py
+++ object-detection/preprocess.py
@@ -1,5 +1,6 @@
 # file này xử lý bộ coil100 sau khi tải về và chia thành train, test
-# trước khi chạy file này thì dùng command line để tạo cây thư mục dạng chuẩn
+# trước khi chạy file này thì dùng command line để tạo cây thư mục dạng chuẩn: mkdir -p coil-100-BOW/{train/obj{1..100},test/obj{1..100}}
+
 # coil-100-BOW
 #     train
 #         obj1
@@ -31,22 +32,26 @@ file_names = os.listdir('coil-100-original')
 # 'obj91', 'obj92', 'obj93', 'obj94', 'obj95', 'obj96', 'obj97', 'obj98', 'obj99', 'obj100']
 
 
-
 train_test_count = 0
+
+
 def get_class_name(file_name):
     pos = file_name.find('_')
     res = file_name[:pos]
     return res
+
 
 # phân chia vào folder train/test tỉ lệ 80/20
 for file_name in file_names:
     img = cv2.imread(os.path.join('coil-100-original', file_name))
     class_name = get_class_name(file_name)
     if train_test_count < 58:
-        cv2.imwrite(os.path.join('coil-100-BOW/train', class_name, file_name), img)
+        cv2.imwrite(os.path.join('coil-100-BOW/train',
+                    class_name, file_name), img)
         train_test_count += 1
     else:
-        cv2.imwrite(os.path.join('coil-100-BOW/test', class_name, file_name), img)
+        cv2.imwrite(os.path.join('coil-100-BOW/test',
+                    class_name, file_name), img)
         train_test_count += 1
         if train_test_count == 72:
             train_test_count = 0
