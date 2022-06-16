2d511d573d7506eaaca7810d047fb608e256fc3c
thanglong2000pro <laphungtien2000@gmail.com>
2022-06-16 23:04:51 +0700

    train and save model

--- object-detection/train.py
+++ object-detection/train.py
@@ -4,7 +4,12 @@
 # 4. Phân loại
 
 
-
+import joblib
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from scipy.cluster.vq import kmeans, vq
 import cv2
 import numpy as np
 import os
@@ -12,7 +17,7 @@ import time
 
 # 1 Đọc. ảnh vào và tìm các descriptor
 # path tới folder ảnh train
-train_path = 'coil-100-BOW/train'
+train_path = 'object-detection/data_process/coil-100-BOW/train'
 # list các classes
 classes_name = os.listdir(train_path)
 
@@ -21,8 +26,10 @@ image_paths = []
 image_classes = []
 class_id = 0
 
+
 def imlist(path):
-    return [os.path.join(path,f) for f in os.listdir(path)]
+    return [os.path.join(path, f) for f in os.listdir(path)]
+
 
 for class_name in classes_name:
     dir = os.path.join(train_path, class_name)
@@ -46,7 +53,7 @@ for image_path in image_paths:
     kpts, des = sift.detectAndCompute(im, None)
     des_list.append((image_path, des))
 t2 = time.time()
-print("Done feature extraction in %d seconds" %(t2-t1))
+print("Done feature extraction in %d seconds" % (t2-t1))
 
 # stack các descriptor lại để phân cụm
 descriptors = des_list[0][1]
@@ -58,14 +65,13 @@ descriptors_float = descriptors.astype(float)
 
 # 2. Phân cụm các descriptor
 # phân cụm các descriptor
-from scipy.cluster.vq import kmeans, vq
 
 # phân thành 500 cụm, giá trị voc trả về là 500 center của 500 cụm
 k = 500
 t3 = time.time()
 voc, variance = kmeans(descriptors_float, k, 1)
 t4 = time.time()
-print("Done clustering in %d seconds" %(t4-t3))
+print("Done clustering in %d seconds" % (t4-t3))
 
 
 # 3. Xây dựng tập BOW(tần suất xuất hiện của các cụm đã phân cụm ở trên ở từng ảnh)
@@ -79,16 +85,12 @@ for i in range(len(image_paths)):
             im_features[i][w] += 1
 
 # chuẩn hóa histogram feature về mean = 0 và std = 1
-from sklearn.preprocessing import StandardScaler
 stdSlr = StandardScaler().fit(im_features)
 im_features = stdSlr.transform(im_features)
 
 # 4. Phân loại
 # sử dụng SVM để phân train
 # Tìm ra bộ tham số tốt nhất
-from sklearn.svm import LinearSVC
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
 param_grid = {'C': [0.1, 1, 10, 100, 1000],
               'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
               'kernel': ['rbf', 'linear']}
@@ -106,10 +108,11 @@ param_grid = {'C': [0.1, 1, 10, 100, 1000],
 #         6: (807 / (7 * 142))
 #     }
 t5 = time.time()
-grid = GridSearchCV(SVC(), param_grid, refit = True, verbose = 3, scoring= 'accuracy', n_jobs=-1)
+grid = GridSearchCV(SVC(), param_grid, refit=True,
+                    verbose=3, scoring='accuracy', n_jobs=-1)
 grid.fit(im_features, np.array(image_classes))
 t6 = time.time()
-print("Done classify in %d seconds" %(t6-t5))
+print("Done classify in %d seconds" % (t6-t5))
 
 
 # in ra bộ tham số tốt nhât
@@ -118,6 +121,5 @@ print(grid.best_params_)
 print(grid.best_score_)
 
 # lưu lại mô hình
-import joblib
-joblib.dump((grid.best_estimator_, classes_name, stdSlr, k, voc), "sift500_coil100.pkl", compress=3)
-
+joblib.dump((grid.best_estimator_, classes_name, stdSlr, k, voc),
+            "sift500_coil100.pkl", compress=3)
